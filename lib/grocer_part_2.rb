5cdea0e23a7e04727daaee711447ce78a153506c
Matt Bugay <mjbugay@yahoo.com>
2020-09-28 01:51:10 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,6 +1,7 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
+<<<<<<< HEAD
   coupons_index = 0
 
   while coupons_index < coupons.size do
@@ -21,6 +22,27 @@ def apply_coupons(cart, coupons)
 end	
 
 
+=======
+  
+  coupons.each do |coupon| 
+    coupon.each do |attribute, value| 
+      name = coupon[:item] 
+    
+      if cart[name] && cart[name][:count] >= coupon[:num] 
+        if cart["#{name} W/COUPON"] 
+          cart["#{name} W/COUPON"][:count] += 1 
+        else 
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
+          :clearance => cart[name][:clearance], :count => 1} 
+        end 
+  
+      cart[name][:count] -= coupon[:num] 
+    end 
+  end 
+end 
+  cart 
+end
+>>>>>>> ace69180824f3a649754b18e405b1629dc37e237
 
 def apply_clearance(cart)
   cart_index = 0
